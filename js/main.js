941d8fee58fa91716bb59c356ce909f5b1362e74
fernandocastanon <fernandocastanon777@gmail.com>
2018-05-12 21:53:22 -0700

    upload site so far

--- js/main.js
+++ js/main.js
@@ -6,8 +6,17 @@ $(document).ready(function() {
     });
 
     var typed = new Typed(".text", {
-    strings: ["<strong>I Love</strong><strong class='primary'> Coding<strong>", "and share"],
-    typespeed: 1,
+    strings: ["<strong>I Love</strong><strong class='primary'> Coding<strong>", "<strong>I Love Making</strong><strong class='primary'> Business Plans<strong>"],
+    typespeed: 2,
     loop: true
-});
+    });
+
+    $(window).scroll(function() {
+      var top = $(window).scrollTop();
+      if (top >= 60) {
+        $('nav').addClass('secondary');
+      } else if ($('nav').hasClass('secondary')) {
+        $('nav').removeClass('secondary');
+      }
+    });
 });
