cb9f077d7c0ac045ba81885b277f299a71b7f22a
fernandocastanon <fernandocastanon777@gmail.com>
2018-06-12 16:38:50 -0700

    make better

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     });
 
     var typed = new Typed(".text", {
-    strings: ["<strong>I Love</strong><strong class='primary'> Coding<strong>", "<strong>I Love Making</strong><strong class='primary'> Business Plans<strong>"],
+    strings: ["<strong>I Love</strong><strong class='primary'> Coding<strong>", "<strong>Learning</strong><strong class='primary'> Digital Marketing<strong>", "<strong>Making Atristic</strong><strong class='primary'> Graphic Designs<strong>"],
     typespeed: 2,
     loop: true
     });
@@ -18,5 +18,31 @@ $(document).ready(function() {
       } else if ($('nav').hasClass('secondary')) {
         $('nav').removeClass('secondary');
       }
+    });
+
+    $('.work').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery: {
+      enabled: true
+    }
+    });
+    $("#team-members").owlCarousel({
+      items:3,
+      autoplay:true,
+      smartSpeed:700,
+      loop:true,
+      autoplayHoverPause:true,
+        responsive:{
+          0:{
+            items:1
+          },
+          480:{
+            items:2
+          },
+          760:{
+            items:3
+          }
+        }
     });
 });
